c14ee28621253bc1047235b61ff535c867658867
dmitriano <winappdev@gmail.com>
2016-12-01 21:32:04 +0300

    Add project files.

---
+++ TopClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TopClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clientTask1 = Client();
+            Console.ReadLine();
+        }
+
+        static async Task Client()
+        {
+            ClientWebSocket ws = new ClientWebSocket();
+            Console.WriteLine("Press return to exit the client.");
+            var uri = new Uri("ws://localhost:8000/ws/");
+
+            await ws.ConnectAsync(uri, CancellationToken.None);
+            var buffer = new byte[1024];
+            while (true)
+            {
+                var segment = new ArraySegment<byte>(buffer);
+
+                var result = await ws.ReceiveAsync(segment, CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await ws.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "I don't do binary", CancellationToken.None);
+                    return;
+                }
+
+                int count = result.Count;
+                while (!result.EndOfMessage)
+                {
+                    if (count >= buffer.Length)
+                    {
+                        await ws.CloseAsync(WebSocketCloseStatus.InvalidPayloadData, "That's too long", CancellationToken.None);
+                        return;
+                    }
+
+                    segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
+                    result = await ws.ReceiveAsync(segment, CancellationToken.None);
+                    count += result.Count;
+                }
+
+                var message = Encoding.UTF8.GetString(buffer, 0, count);
+                Console.WriteLine(">" + message);
+            }
+
+        }
+    }
+}
---
+++ TopServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TopServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1 = Run();
+
+            task1.Wait();
+
+            Console.WriteLine("Press return to exit the server.");
+            Console.ReadLine();
+        }
+        public static async Task Run()
+        {
+            HttpListener s = new HttpListener();
+            s.Prefixes.Add("http://localhost:8000/ws/");
+            s.Start();
+
+            var hc = await s.GetContextAsync();
+            if (!hc.Request.IsWebSocketRequest)
+            {
+                hc.Response.StatusCode = 400;
+                hc.Response.Close();
+                return;
+            }
+            var wsc = await hc.AcceptWebSocketAsync(null);
+            var ws = wsc.WebSocket;
+
+            for (int i = 0; i != 10; ++i)
+            {
+                await Task.Delay(1000);
+                var time = DateTime.Now.ToLongTimeString();
+                var buffer = Encoding.UTF8.GetBytes(time);
+                var segment = new ArraySegment<byte>(buffer);
+                await ws.SendAsync(segment, System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Done", CancellationToken.None);
+        }
+    }
+}
