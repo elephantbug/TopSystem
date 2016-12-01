c4fe23ba90f5c158ccf949c50ed87da385581e7a
dmitriano <winappdev@gmail.com>
2016-12-01 21:57:48 +0300

    tasks on the server

--- TopServer/Program.cs
+++ TopServer/Program.cs
@@ -13,38 +13,46 @@ namespace TopServer
     {
         static void Main(string[] args)
         {
-            var task1 = Run();
+            Run();
 
-            task1.Wait();
+            //task1.Wait();
 
             Console.WriteLine("Press return to exit the server.");
             Console.ReadLine();
         }
-        public static async Task Run()
+        public static void Run()
         {
             HttpListener s = new HttpListener();
             s.Prefixes.Add("http://localhost:8000/ws/");
             s.Start();
 
-            var hc = await s.GetContextAsync();
-            if (!hc.Request.IsWebSocketRequest)
-            {
-                hc.Response.StatusCode = 400;
-                hc.Response.Close();
-                return;
-            }
-            var wsc = await hc.AcceptWebSocketAsync(null);
-            var ws = wsc.WebSocket;
+            var listen_task = s.GetContextAsync();
 
-            for (int i = 0; i != 10; ++i)
+            listen_task.ContinueWith(async (hc_task) => 
             {
-                await Task.Delay(1000);
-                var time = DateTime.Now.ToLongTimeString();
-                var buffer = Encoding.UTF8.GetBytes(time);
-                var segment = new ArraySegment<byte>(buffer);
-                await ws.SendAsync(segment, System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
-            }
-            await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Done", CancellationToken.None);
+                var hc = hc_task.Result;
+
+                if (!hc.Request.IsWebSocketRequest)
+                {
+                    hc.Response.StatusCode = 400;
+                    hc.Response.Close();
+                    return;
+                }
+
+                var wsc = await hc.AcceptWebSocketAsync(null);
+
+                var ws = wsc.WebSocket;
+
+                for (int i = 0; i != 10; ++i)
+                {
+                    await Task.Delay(1000);
+                    var time = DateTime.Now.ToLongTimeString();
+                    var buffer = Encoding.UTF8.GetBytes(time);
+                    var segment = new ArraySegment<byte>(buffer);
+                    await ws.SendAsync(segment, System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Done", CancellationToken.None);
+            });
         }
     }
 }
